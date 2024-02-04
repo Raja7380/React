362b0392d65e3886ef3da04c75376fd1f71c7d5f
Raja Singh <Rajasingh125987@gmail.com>
2024-02-04 13:08:56 +0530

    first commit

---
+++ 2/app.js
@@ -0,0 +1,25 @@
+const heading = React.createElement("div", {id:"parent"}, [
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"Hi I am H1"),
+        React.createElement("h1",{},"Hi I am H2")
+    ])
+],React.createElement("div",{id:"child2"},[
+    React.createElement("h1",{},"Hi I am H1"),
+    React.createElement("h1",{},"Hi I am H2")
+]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
